2b1a3467f61230bff17c3f1114ba19d9bb21002d
rodrigoFM17 <rodrig4243@gmail.com>
2023-11-03 00:22:16 -0600

    todo jalando ya al 100

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,20 +1,23 @@
 const bcrypt = require('bcrypt')
 const jwtToken = process.env.JWTSECRET
 const db = require('../configs/db')
+const jwt = require('jsonwebtoken')
 
 
-const login = async () =>{
+const login = async (req, res) =>{
     try {
         const {email, password} = req.body;
 
-        const usuarioEncontrado = await db.execute('select * from usuario where email = ?', [email]);
-        if (!usuarioEncontrado[0]) {
+        const usuarioEncontrado = await db.execute('select * from usuarios where email = ?', [email]);
+        //return res.json(usuarioEncontrado[0][0])
+        if (usuarioEncontrado[0].length == 0) {
             return res.status(400).json({
                 message: "email o password incorrecto"
             });
         }
 
-        const passwordCorrecto = bcrypt.compareSync(password, usuarioEncontrado[0].password);
+        const passwordCorrecto = bcrypt.compareSync(password, usuarioEncontrado[0][0].password);
+        //return res.json(passwordCorrecto)
         if (!passwordCorrecto) {
             return res.status(400).json({
                 message: "email o password incorrecto"
@@ -23,7 +26,7 @@ const login = async () =>{
 
         const payload = {
             usuario: {
-                id: usuarioEncontrado.id
+                id: usuarioEncontrado[0][0].id
             }
         }
 
@@ -39,4 +42,8 @@ const login = async () =>{
             error: error.message
         });
     }
+}
+
+module.exports = {
+    login
 }
--- src/routes/usuarios.route.js
+++ src/routes/usuarios.route.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const usuariosController = require('../controllers/usuarios.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
+const authController = require('../controllers/auth.controller')
 
-
+router.get('/login', authController.login)
 router.get('/',authMiddleware.verificarJWT, usuariosController.index)
 router.get('/:id',authMiddleware.verificarJWT, usuariosController.getById)
 router.post('/',authMiddleware.verificarJWT, usuariosController.create)
 router.patch('/:id',authMiddleware.verificarJWT, usuariosController.updateParcial)
 router.put('/:id',authMiddleware.verificarJWT, usuariosController.updateCompleto)
 router.delete('/:id',authMiddleware.verificarJWT, usuariosController.delete)
+
 
 module.exports = router
